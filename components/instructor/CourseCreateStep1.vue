cad23639740dd1e1fe9e1e371d2f94a872719553
Filip Jerga <jerga99@gmail.com>
2019-07-28 22:34:37 +0200

    28 Separate Course Create into multiple components

---
+++ components/instructor/CourseCreateStep1.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="course-create-wrapper">
+    <div class="course-create-headerText">
+      Please choose title of your Course.
+    </div>
+    <h2 class="course-create-subtitle">
+      No worries, you can change title later.
+    </h2>
+    <form @input="emitFormData" class="course-create-form">
+      <div class="course-create-form-group">
+        <div class="field course-create-form-field control has-icons-right">
+          <input
+            :maxLength="50"
+            type="text"
+            placeholder="e.g. Amazing Course in Flutter!"
+            class="input is-large">
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
---
+++ components/instructor/CourseCreateStep2.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="course-create-wrapper">
+    <div class="course-create-headerText">
+      What category best fits the knowledge you'll share?
+    </div>
+    <h2 class="course-create-subtitle">If you're not sure about the right category, you can change it later.</h2>
+    <form class="course-create-form">
+      <div class="course-create-form-group">
+        <div class="course-create-form-field">
+          <div class="select is-large">
+            <select>
+              <option value="default">Select Category</option>
+              <option>
+                Mobile Development
+              </option>
+              <option>
+                Web Development
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
