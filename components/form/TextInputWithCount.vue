49fde5369ffaed1ea74de4bf5b3e6f8491df7698
Filip Jerga <jerga99@gmail.com>
2019-07-29 20:24:45 +0200

    38 Init text input with count component

---
+++ components/form/TextInputWithCount.vue
@@ -0,0 +1,19 @@
+<template>
+  <input
+    :maxLength="maxLength"
+    type="text"
+    placeholder="e.g. Amazing Course in Flutter!"
+    class="input is-large">
+</template>
+
+<script>
+export default {
+  props: {
+   maxLength: {
+    type: Number,
+    default: 50,
+    required: false
+   }
+  }
+}
+</script>
--- components/instructor/CourseCreateStep1.vue
+++ components/instructor/CourseCreateStep1.vue
@@ -9,14 +9,7 @@
     <form class="course-create-form">
       <div class="course-create-form-group">
         <div class="field course-create-form-field control has-icons-right">
-          <input
-            @input="emitFormData"
-            @blur="$v.form.title.$touch()"
-            v-model="form.title"
-            :maxLength="50"
-            type="text"
-            placeholder="e.g. Amazing Course in Flutter!"
-            class="input is-large">
+          <TextInputWithCount :maxLength="50" />
           <div v-if="$v.form.title.$error" class="form-error">
             <span v-if="!$v.form.title.required" class="help is-danger">Title is required!</span>
           </div>
@@ -28,7 +21,11 @@
 
 <script>
 import { required } from 'vuelidate/lib/validators'
+import TextInputWithCount from '~/components/form/TextInputWithCount';
 export default {
+  components: {
+    TextInputWithCount
+  },
   data() {
     return {
       form: {
