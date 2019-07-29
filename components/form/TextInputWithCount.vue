e1267d381a31e848fd79734bf4ce385360c4dc83
Filip Jerga <jerga99@gmail.com>
2019-07-29 22:23:15 +0200

    40 Finished input with character counter

--- components/form/TextInputWithCount.vue
+++ components/form/TextInputWithCount.vue
@@ -1,25 +1,66 @@
 <template>
-  <input
-    :maxLength="maxLength"
-    @input="$event => $emit('input', $event.target.value)"
-    @blur="v.$touch()"
-    type="text"
-    placeholder="e.g. Amazing Course in Flutter!"
-    class="input is-large">
+  <div>
+    <input
+      :maxLength="maxLength"
+      @input="emitInputValue"
+      @blur="v.$touch()"
+      type="text"
+      placeholder="e.g. Amazing Course in Flutter!"
+      class="input is-large">
+    <span class="icon is-small is-right">
+      {{remainingLength}}
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      currentValue: ''
+    }
+  },
   props: {
-   maxLength: {
-    type: Number,
-    default: 50,
-    required: false
-   },
-   v: {
-    type: Object,
-    required: true
-   }
+    maxLength: {
+      type: Number,
+      default: 50,
+      required: false
+    },
+    v: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    inputLength() {
+      return this.currentValue.length || 0
+    },
+    remainingLength() {
+      if (this.inputLength > 0 && this.inputLength < this.maxLength) {
+        return this.maxLength - this.inputLength
+      } else if (this.inputLength === 0) {
+        return this.maxLength
+      } else {
+        return 0
+      }
+    }
+  },
+  methods: {
+    emitInputValue($event) {
+      this.currentValue = $event.target.value
+      this.$emit('input', this.currentValue)
+    }
   }
 }
 </script>
+
+
+
+
+
+
+
+
+
+
+
