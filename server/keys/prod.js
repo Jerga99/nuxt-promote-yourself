a166be64bbc8951b0760b61a0bfb0c41d2cb05c6
Filip Jerga <jerga99@gmail.com>
2019-08-14 21:40:26 +0200

    117 Deploy part 1

--- server/keys/prod.js
+++ server/keys/prod.js
@@ -1,4 +1,11 @@
+// module.exports = {
+//   DB_URI: process.env.DB_URI,
+//   SESSION_SECRET: process.env.SESSION_SECRET
+// }
+
+
 module.exports = {
-  DB_URI: process.env.DB_URI,
-  SESSION_SECRET: process.env.SESSION_SECRET
+  DB_URI: 'mongodb+srv://testuser:testtest@cluster0-abvl2.mongodb.net/promo-db?retryWrites=true&w=majority',
+  SESSION_SECRET: "ada7d9a7?d<:|"
 }
+
