e8c78ae590d3dab4d560840008ea590662aa9610
Filip Jerga <jerga99@gmail.com>
2019-08-14 21:58:25 +0200

    118 Prod changes part 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,6 +65,7 @@ module.exports = {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
+    baseURL: process.env.BASE_URL || 'http://localhost:3000'
   },
   serverMiddleware: [
     '~/server/routes/index'
--- server/keys/prod.js
+++ server/keys/prod.js
@@ -1,11 +1,4 @@
-// module.exports = {
-//   DB_URI: process.env.DB_URI,
-//   SESSION_SECRET: process.env.SESSION_SECRET
-// }
-
-
 module.exports = {
-  DB_URI: 'mongodb+srv://testuser:testtest@cluster0-abvl2.mongodb.net/promo-db?retryWrites=true&w=majority',
-  SESSION_SECRET: "ada7d9a7?d<:|"
+  DB_URI: process.env.DB_URI,
+  SESSION_SECRET: process.env.SESSION_SECRET
 }
-
